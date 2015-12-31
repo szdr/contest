f6c7d814c398a1995e61b206dcd0899dac351b5b
ShogoSuzuki <s_suzuki@bi.cs.titech.ac.jp>
2015-12-31 19:18:36 +0900

    Add extgcd.cpp in math

---
+++ math/extgcd.cpp
@@ -0,0 +1,12 @@
+// ax+by=gcd(a,b)
+
+int extgcd(int a, int b, int &x, int &y) {
+    int d = a;
+    if (b != 0) {
+        d = extgcd(b, a % b, y, x);
+        y -= (a / b) * x;
+    } else {
+        x = 1; y = 0;
+    }
+    return d;
+}
