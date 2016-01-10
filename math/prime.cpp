3ccc53d97365305b1fc6cdd4931bb00bbd954c78
ShogoSuzuki <s_suzuki@bi.cs.titech.ac.jp>
2016-01-10 23:55:29 +0900

    Add segment_sieve

--- math/prime.cpp
+++ math/prime.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <map>
 #define MAX_N 1000
+typedef long long ll;
 using namespace std;
 
 // naive prime test
@@ -63,3 +64,27 @@ int sieve(int n) {
     return p;
 }
 
+// segment Eratosthenes sieve[a, b)
+// is_prime_seg_list[i - a] = true means that i is prime
+#define MAX_L 1000
+#define MAX_SQRT_B 1000
+bool is_prime_seg_list[MAX_L];
+bool is_prime_small_list[MAX_SQRT_B];
+void segment_sieve(ll a, ll b) {
+    for (int i = 0; (ll)i * i < b; i++) {
+        is_prime_small_list[i] = true;
+    }
+    for (int i = 0; i < b - a; i++) {
+        is_prime_seg_list[i] = true;
+    }
+    for (int i = 2; (ll)i * i < b; i++) {
+        if (is_prime_small_list[i]) {
+            for (int j = 2 * i; (ll)j * j < b; j += i) {
+                is_prime_small_list[j] = false;
+            }
+            for (ll j = max(2LL, (a + i - 1) / i) * i; j < b; j+= i) {
+                is_prime_seg_list[j - a] = false;
+            }
+        }
+    }
+}
