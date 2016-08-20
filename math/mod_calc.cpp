b702e029eb5f5c28223371ee232e14996ef4d7fa
ShogoSuzuki <s_suzuki@bi.cs.titech.ac.jp>
2016-08-21 03:26:09 +0900

    ll -> LL

--- math/mod_calc.cpp
+++ math/mod_calc.cpp
@@ -1,31 +1,31 @@
-typedef long long ll;
+typedef long long LL;
 
 // a+b (mod p)
-ll add_mod(int a, int b, int p) {
-    ll d = a + b;
+LL add_mod(int a, int b, int p) {
+    LL d = a + b;
     return d % p;
 }
 
 // a-b (mod p)
-ll sub_mod(int a, int b, int p) {
-    ll d = a + p;
+LL sub_mod(int a, int b, int p) {
+    LL d = a + p;
     d -= b;
     return d % p;
 }
 
 // a*b (mod p)
-ll mul_mod(int a, int b, int p) {
-    ll d = (a % p) * (b % p);
+LL mul_mod(int a, int b, int p) {
+    LL d = (a % p) * (b % p);
     return d;
 }
 
 // a^b (mod p)
-ll power_mod(int a, int b, int p) {
+LL power_mod(int a, int b, int p) {
     if (b == 0) {
         return 1;
     }
     if (b % 2 == 0) {
-        ll d = power_mod(a, b / 2, p);
+        LL d = power_mod(a, b / 2, p);
         return (d * d) % p;
     } else {
         return (a * power_mod(a, b - 1, p)) % p;
@@ -35,7 +35,7 @@ ll power_mod(int a, int b, int p) {
 // a \div b (mod p) CAUTION! p must be prime !
 // By Fermat theorem, a^{p-1} \equiv 1 (mod p)
 // so, a^{p-2} \equiv a^{-1} (mod p)
-ll div_mod(int a, int b, int p) {
-    ll pos_p_2 = power_mod(b, p - 2, p);
+LL div_mod(int a, int b, int p) {
+    LL pos_p_2 = power_mod(b, p - 2, p);
     return (a * pos_p_2) % p;
 }
