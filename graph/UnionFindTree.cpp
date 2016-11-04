8b87f329b337393266db1212a7c4ab98848f4ba4
ShogoSuzuki <s_suzuki@bi.cs.titech.ac.jp>
2016-11-05 01:12:35 +0900

    Modify for using struct for UnionFindTree.cpp

--- graph/UnionFindTree.cpp
+++ graph/UnionFindTree.cpp
@@ -1,45 +1,24 @@
-#define MAX_N 1000
-int par[MAX_N]; // parent
-int rank[MAX_N]; // depth
-
-// initialize with n elements
-void init(int n) {
-    for (int i = 0; i < n; i++) {
-        par[i] = i;
-        rank[i] = 0;
-    }
-}
-
-
-// find the root of tree
-int find(int x) {
-    if (par[x] == x) {
-        return x;
-    } else {
-        return par[x] = find(par[x]);
-    }
-}
-
-
-// unite two sets
-// a set contains x and the other set contains y
-void unite(int x, int y) {
-    x = find(x);
-    y = find(y);
-    if (x == y) {
-        return;
-    }
-    if (rank[x] < rank[y]) {
-        par[x] = y;
-    } else {
-        par[y] = x;
-        if (rank[x] == rank[y]) {
-            rank[x]++;
+#include <vector>
+using namespace std;
+struct UnionFind {
+    vector<int> par;
+    int n, cnt;
+    UnionFind(const int& x = 0) {init(x);}
+    void init(const int& x) {par.assign(cnt=n=x, -1);}
+    inline int find(const int& x) {return par[x] < 0 ? x : par[x] = find(par[x]);}
+    inline bool same(const int& x, const int& y) { return find(x) == find(y); }
+    inline bool unite(int x, int y) {
+        if ((x = find(x)) == (y = find(y))) {
+            return false;
         }
+        --cnt;
+        if (par[x] > par[y]) {
+            swap(x, y);
+        }
+        par[x] += par[y];
+        par[y] = x;
+        return true;
     }
-}
-
-// whether x and y are included in the same set
-bool same(int x, int y) {
-    return find(x) == find(y);
-}
+    inline int count() const {return cnt;}
+    inline int count(int x) {return -par[find(x)];}
+};
