59edfc4d684f689617033cb8218285db48db46ec
ShogoSuzuki <s_suzuki@bi.cs.titech.ac.jp>
2016-11-05 01:13:52 +0900

    Add matrix.cpp

---
+++ math/matrix.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+using namespace std;
+typedef vector<int> vec;
+typedef vector<vec> mat;
+
+mat mul(mat &A, mat &B) {
+    mat C(A.size(), vec(B[0].size()));
+    for (int i = 0; i < A.size(); i++) {
+        for (int k = 0; k < B.size(); k++) {
+            for (int j = 0; j < B[0].size(); j++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+    return C;
+}
+
+// A^n
+mat pow(mat A, int n) {
+    mat B(A.size(), vec(A.size()));
+    for (int i = 0; i < A.size(); i++) {
+        B[i][i] = 1;
+    }
+    while (n > 0) {
+        if (n & 1) {
+            B = mul(B, A);
+        }
+        A = mul(A, A);
+        n >>= 1;
+    }
+    return B;
+}
