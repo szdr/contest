94b234be7e4fcca380388bb9646f651457025b5a
ShogoSuzuki <s_suzuki@bi.c.titech.ac.jp>
2017-07-02 11:54:41 +0900

    [update] combination

--- math/mod_calc.cpp
+++ math/mod_calc.cpp
@@ -1,63 +1,91 @@
+#include<vector>
+#include<iostream>
+using namespace std;
 typedef long long LL;
-const int MOD = 1e9 + 7;
 
-// a+b (mod p)
-LL add_mod(LL a, LL b) {
-    LL d = (a % MOD) + (b % MOD) % MOD;
+// a+b
+LL add_mod(LL a, LL b, LL mod) {
+    LL d = (a % mod) + (b % mod) % mod;
     return d;
 }
 
-// a-b (mod p)
-LL sub_mod(LL a, LL b) {
-    LL d = (a % MOD - b % MOD + MOD) % MOD;
+// a-b
+LL sub_mod(LL a, LL b, LL mod) {
+    LL d = (a % mod - b % mod + mod) % mod;
     return d;
 }
 
-// a*b (mod p)
-LL mul_mod(LL a, LL b) {
-    LL d = (a % MOD) * (b % MOD) % MOD;
+// a*b
+LL mul_mod(LL a, LL b, LL mod) {
+    LL d = (a % mod) * (b % mod) % mod;
     return d;
 }
 
-// a^b (mod p)
-LL power_mod(LL a, LL b) {
-    if (b == 0) {
-        return 1;
+// (base)^p
+LL power_mod(LL base, LL p, LL mod) {
+    LL ans = 1;
+    while (p > 0) {
+        if (p % 2 == 1) {
+            ans = (ans * base) % mod;
+        }
+        base = (base * base) % mod;
+        p /= 2;
     }
-    if (b % 2 == 0) {
-        LL d = power_mod(a, b / 2);
-        return (d * d) % MOD;
-    } else {
-        return (a * power_mod(a, b - 1)) % MOD;
-    }
+    return ans;
 }
 
 // a \div b (mod p) CAUTION! p must be prime !
 // By Fermat theorem, a^{p-1} \equiv 1 (mod p)
 // so, a^{p-2} \equiv a^{-1} (mod p)
-LL div_mod(LL a, LL b) {
-    LL pos_p_2 = power_mod(b, MOD - 2);
-    return (a * pos_p_2) % MOD;
+LL div_mod(LL a, LL b, LL mod) {
+    LL pos_p_2 = power_mod(b, mod - 2, mod);
+    return (a * pos_p_2) % mod;
 }
 
-const int MAX = 100;
-LL fact[MAX];
-void fact_mod(int n) {
-    for (int i = 0; i <= n; i++) {
-        if (i == 0) {
-            fact[i] = 1;
-        } else {
-            fact[i] = fact[i-1] * i % MOD;
-        }
+vector<LL> fact_mod(int n, LL mod) {
+    vector<LL> fact(n + 1, 1);
+    for (int i = 1; i <= n; i++) {
+        fact[i] = (fact[i - 1] * i) % mod;
     }
+
+    return fact;
 }
 
-LL comb_mod(LL n, LL r) {
-    fact_mod(n);
-    LL res = fact[n];
-    LL a = power_mod(fact[n - r], MOD - 2);
-    LL b = power_mod(fact[r], MOD - 2);
-    res = mul_mod(res, a);
-    res = mul_mod(res, b);
-    return res;
+vector<LL> rfact_mod(int n, LL mod) {
+    vector<LL> rfact(n + 1, 1);
+    vector<LL> fact = fact_mod(n, mod);
+    for (int i = 1; i <= n; i++) {
+        rfact[i] = power_mod(fact[i], mod - 2, mod);
+    }
+    return rfact;
+}
+
+/*
+ * WARNING: fact_modとrfact_modの事前計算が必要
+ */
+LL ncr_mod(LL n, LL r, vector<LL> fact, vector<LL> rfact, LL mod) {
+    if (r > n || n < 0 || r < 0) {
+        return 0;
+    }
+    LL ans = (fact[n] * rfact[r]) % mod;
+    ans = (ans * rfact[n - r]) % mod;
+    return ans;
+}
+
+
+int main(int argc, char const* argv[])
+{
+    const int MOD = 1e9 + 7;
+    vector<LL> fact = fact_mod(100000, MOD);
+    vector<LL> rfact = rfact_mod(100000, MOD);
+
+    LL ans1 = ncr_mod(8, 4, fact, rfact, MOD);
+    cout << "Expected: " << 70 << endl;
+    cout << "Actual: " << ans1 << endl;
+
+    LL ans2 = ncr_mod(50, 20, fact, rfact, MOD);
+    cout << "Expected: " << 211914057 << endl;
+    cout << "Actual: " << ans2 << endl;
+
+    return 0;
 }
